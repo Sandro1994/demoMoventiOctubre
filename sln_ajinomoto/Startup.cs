e30876e270a5d53dfd0ebd5ffdca9e06633618f6
sarzapalo94 <sarzapalosupanta@gmail.com>
2024-10-04 13:44:49 -0500

    feat: task Creación, configuración y levantamiento proyecto ASP NET

---
+++ sln_ajinomoto/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Security.Jwt;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text; 
+using System.Web;
+
+namespace RetoTecnicoAjinomoto
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var key = Encoding.ASCII.GetBytes("HFmNLA8TSttPzq6lVGlG");
+
+            app.UseJwtBearerAuthentication(new JwtBearerAuthenticationOptions
+            {
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                }
+            });
+        }
+    }
+}
